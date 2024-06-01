b04f0ed185d99fdbafd60af57e24722cbf582fda
satwik0224 <156293192+satwik0224@users.noreply.github.com>
2024-06-01 12:09:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,60 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = var.aks_cluster_name
 
   default_node_pool {
+    name       = "default"# Define Azure provider
+provider "azurerm" {
+  features {}
+}
+ 
+# Define resource group
+resource "azurerm_resource_group" "revhire_rg" {
+  name     = "team4"
+  location = "East US"  # Choose a region that supports free tier AKS
+}
+ 
+ 
+# Create Azure Kubernetes Service (AKS) cluster
+resource "azurerm_kubernetes_cluster" "revhire_aks" {
+  name                = "revhire-aks-cluster"
+  location            = azurerm_resource_group.revhire_rg.location
+  resource_group_name = azurerm_resource_group.revhire_rg.name
+  dns_prefix          = "revhireaks"
+ 
+  default_node_pool {
     name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"  # Use Standard_B2s for free trial
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
+  }
+ 
+  identity {
+    type = "SystemAssigned"
+  }
+ 
+  network_profile {
+    network_plugin = "azure"
+    dns_service_ip = "10.2.0.10"
+    service_cidr   = "10.2.0.0/16"
+  }
+}
+ 
+# Create Azure Container Registry (ACR)
+resource "azurerm_container_registry" "revhire_acr" {
+  name                = "revhireacrsatwik2402"
+  resource_group_name = azurerm_resource_group.revhire_rg.name
+  location            = azurerm_resource_group.revhire_rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+ 
+# Assign AcrPull role to AKS
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.revhire_acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.revhire_aks.kubelet_identity[0].object_id
+}
+ 
+ 
     node_count = 2
     vm_size    = "Standard_DS2_v2"
   }
